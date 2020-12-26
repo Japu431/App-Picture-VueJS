8bc2aaa09cf63abc4a927bb587b5b511ba8958af
Yusuke-kun <japamnk@gmail.com>
2020-12-26 17:34:04 -0300

    adapting the template

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="(foto, i) of fotos" :key="i">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: "AluraPic",
+      fotos: [
+        {
+          url:
+            "https://www.petz.com.br/blog/wp-content/uploads/2019/04/temperatura-cachorro.jpg",
+          titulo: "Cachorro",
+        },
+        {
+          url:
+            "https://www.petz.com.br/blog/wp-content/uploads/2019/04/temperatura-cachorro.jpg",
+          titulo: "Cachorrão",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style
+
+</style>
